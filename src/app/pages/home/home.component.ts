71cecabf9d36da1262e4078a94f5bdd919b1a838
israel mailman <80362556+likefools@users.noreply.github.com>
2021-11-07 00:50:08 +0200

    0.2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  title: string = 'my website';
+  textHeader: string = 'האתר הטוב ביותר';
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
